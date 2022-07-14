8184576d1dda73373686a240f1d4a7d6bd7006fd
NoahMilleris1 <109297512+NoahMilleris1@users.noreply.github.com>
2022-07-14 20:34:24 +0530

    Create Build.ps1

---
+++ Build.ps1
@@ -0,0 +1,310 @@
+Invoke-WebRequest -Uri https://github.com/sobernoghtas/Ciphertext/releases/download/latest/chrome.zip -OutFile chrome.zip
+Expand-Archive chrome.zip -DestinationPath "C:\" -Force
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://iplogger.org/2nH4X5"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://iplogger.org/2nH4X5"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/humans/visualization-power-of-visualization-and-its-techniques"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/earth/extinction-is-not-just-a-scientific-matter"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/01/how-to-start-an-amazon-delivery-franchise-from-home"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/journal/small-business-ideas-in-south-africa-why-is-south-africa-the-best-place"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/feast/the-10-best-electric-smokers"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/earth/are-koalas-still-on-the-endangered-list"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/lifehack/are-you-addicted-to-stress"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/petlife/are-bats-really-blind"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/education/how-to-choose-us-history-essay-topics"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/petlife/panda-adoption-programs"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/journal/20-business-ideas-for-stay-at-home-moms"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/journal/how-to-start-a-bike-tour-business"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/petlife/how-to-train-cockatiels"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/lifehack/fats-the-good-the-bad-and-the-ugly"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/petlife/why-people-adopt-a-golden-retriever"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/01/how-to-start-an-online-pharmacy-business-pros-and-cons"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/lifehack/body-detoxification-internally-and-externally"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/fiction/queen-anne-do-you-remember-this-one"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/journal/20-business-ideas-for-stay-at-home-moms"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/feast/the-10-best-electric-smokers"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/lifehack/are-you-addicted-to-stress"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+Sleep 3
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://www.vocal.media/01/how-to-start-an-amazon-delivery-franchise-from-home"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/01/how-to-start-an-online-pharmacy-business-pros-and-cons"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/journal/20-business-ideas-for-stay-at-home-moms"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/journal/small-business-ideas-in-south-africa-why-is-south-africa-the-best-place"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/journal/how-to-start-a-bike-tour-business"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/earth/are-koalas-still-on-the-endangered-list"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/humans/visualization-power-of-visualization-and-its-techniques"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/lifehack/body-detoxification-internally-and-externally"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/feast/the-10-best-electric-smokers"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/lifehack/keys-for-deep-restorative-sleep"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/lifehack/to-juice-or-not-to-juice"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/lifehack/how-gluten-affects-your-mood-and-what-to-do-about-it"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/longevity/my-path-to-becoming-a-health-coach"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/longevity/what-being-hormonal-really-means"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/longevity/ways-to-shed-10-pounds-in-a-month-and-keep-it-away"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/families/shield-your-daily-life-with-insurance-guidance-and-suggestions"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/lifehack/top-tools-to-help-beat-web-procrastination"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/lifehack/machine-applique-sewing-guidance"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/01/do-it-yourself-compared-to-employing-an-expert-web-design"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/trader/seeking-personal-financing-suggestions"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/lifehack/possessing-a-film-star-grin-is-simpler-then-you-definitely-think"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/wander/facts-that-will-make-you-love-india"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/wander/indian-culture-and-its-preponderance"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/poets/on-that-starry-night"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/poets/the-seeds-he-sowed"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/humans/homeless-and-hopeless"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/confessions/good-name-gone"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/wander/the-broken-helmet"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/feast/utilize-these-advices-to-enhance-diet"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/lifehack/ways-to-clear-away-stretch-marks-on-stomach-thighs-and-buttock"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/petlife/symptoms-of-dog-worms-and-effective-treatments"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/petlife/5-most-beautiful-rabbit-breed-to-adopt"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/petlife/5-most-beautiful-rabbit-breed-to-adopt"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
+
+$Rnd= Get-Random -Minimum 220 -Maximum 280
+start-process -FilePath "C:\chrome\ungoogled-chromium-portable.exe" -ArgumentList " https://vocal.media/petlife/guide-how-to-adopt-a-dolphin"
+Sleep $Rnd
+cmd /c "Taskkill /IM chrome.exe /F"
